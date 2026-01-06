a3311ea235bff09f88f76e71696f702dc29a435c
evol-ishzk-y <ishizuka-y@evoltech.co.jp>
2026-01-06 14:21:45 +0900

    blackjackプロジェクト作成とCardクラスの作成 マークと数字のList型変数を用意

---
+++ blackjack/blackjack/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    internal class Card
+    {
+        //List型の変数を作る(マーク)
+        List<string> marks = new List<string>();
+
+        //List型の変数を作る(数字)
+        List<int> nums = new List<int>();
+    }
+}
