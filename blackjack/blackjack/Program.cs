2f0bc3ae3ffff43be6d099bde13eaa8b94050a36
evol-ishzk-y <ishizuka-y@evoltech.co.jp>
2026-01-06 15:21:12 +0900

    山札を作成

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using blackjack;
+using System;
+
+//Listの山札作成
+List<Card> deck = new List<Card>();
+
+//マークを格納
+string[] markTypes = {"heart", "diamond", "spade", "clover"};
+
+foreach(string m  in markTypes)
+{
+    for(int i = 1; i <= 13; i++ )
+    {
+        Card c = new Card();
+        c.mark = m;
+        c.num = i;
+ 
+        //deckに５２枚のカードをいれる
+        deck.Add(c);
+    }
+}
